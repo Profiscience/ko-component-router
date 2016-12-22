ab0311018a9d2f12bb4291c517ae53851527ddc8
Casey Webb <notcaseywebb@gmail.com>
2016-12-21 22:21:11 -0600

    Fix path binding with nested routing

--- docs/path-binding.md
+++ docs/path-binding.md
@@ -4,27 +4,35 @@ For your convenience, the router includes a binding called `path` that allows
 you to set the href of anchor tags without mucking around with the base path
 and having to juggle different levels too much.
 
-__Local__
+### Local
 ```html
 <a data-bind="path: '/foo'">
 ```
 
-This will route to the `/foo` route on the most immediate router, i.e.
-a router in the same component, or the containing router.
+This will route to the `/foo` route on the current router (the one that this
+page belongs to).
 
-__Absolute__
+### Absolute
 ```html
 <a data-bind="path: '//foo'">
 ```
 
 This will route to the `/foo` route at the top-level router.
 
-__Relative__
+### Relative
+__parent__
 ```html
 <a data-bind="path: '../foo'">
 ```
 
 This will route to the `/foo` route at the parent router.
+
+__child__
+```html
+<a data-bind="path: './foo'">
+```
+
+This will route to the `/foo` route at the child (adjacent) router.
 
 ---
 
--- test/binding.js
+++ test/binding.js
@@ -3,9 +3,9 @@ import $ from 'jquery'
 
 ko.components.register('binding', {
   template: `
-    <a id="root-relative" data-bind="path: '/a'"></a>
-    <a id="root-absolute" data-bind="path: '//a'"></a>
-    <a id="root-deep" data-bind="path: '/a/a'"></a>
+    <a id="outer-relative" data-bind="path: '/a'"></a>
+    <a id="outer-absolute" data-bind="path: '//a'"></a>
+    <a id="outer-deep" data-bind="path: '/a/a'"></a>
     <ko-component-router params="routes: routes"></ko-component-router>
   `,
   viewModel: class BindingTest {
@@ -14,43 +14,52 @@ ko.components.register('binding', {
       history.replaceState(null, null, '/a/a')
 
       this.routes = {
-        '/a/!': 'a'
+        '/a/!': ['a', {
+          '/a': 'a-inner'
+        }]
       }
-
-      ko.components.register('empty', { template: '<div></div>' })
 
       ko.components.register('a', {
         viewModel() {
-          this.routes = { '/a': 'empty' }
-
           setTimeout(() => {
-            t.equals('/a', $('#root-relative').attr('href'))
-            t.equals('/a', $('#root-absolute').attr('href'))
+            t.equals('/a', $('#outer-relative').attr('href'))
+            t.equals('/a', $('#outer-absolute').attr('href'))
+
+            t.equals('/a/a', $('#inner-relative').attr('href'))
+            t.equals('/a', $('#inner-absolute').attr('href'))
 
             t.equals('/a/a', $('#nested-relative').attr('href'))
             t.equals('/a', $('#nested-relative-up').attr('href'))
             t.equals('/a', $('#nested-absolute').attr('href'))
 
-            t.ok($('#root-relative').hasClass('active-path'))
+            t.ok($('#outer-relative').hasClass('active-path'))
+            t.ok($('#inner-relative').hasClass('active-path'))
             t.ok($('#nested-relative').hasClass('active-path'))
-            t.ok($('#root-deep').hasClass('active-path'))
+            t.ok($('#outer-deep').hasClass('active-path'))
 
             history.pushState(null, null, begin)
             next()
           })
         },
         template: `
+          <a id="inner-relative" data-bind="path: './a'"></a>
+          <a id="inner-absolute" data-bind="path: '//a'"></a>
+          <ko-component-router></ko-component-router>
+        `
+      })
+
+      ko.components.register('a-inner', {
+        template: `
           <a id="nested-relative" data-bind="path: '/a'"></a>
           <a id="nested-relative-up" data-bind="path: '../a'"></a>
           <a id="nested-absolute" data-bind="path: '//a'"></a>
-          <ko-component-router params="routes: routes"></ko-component-router>
         `
       })
     }
 
     dispose() {
       ko.components.unregister('a')
-      ko.components.unregister('empty')
+      ko.components.unregister('a-inner')
       ko.components.unregister('binding')
     }
   }
