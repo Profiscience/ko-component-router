236d04750247003c38113fe13dace1851396439e
Matt Hensley <matt.barake@gmail.com>
2017-01-05 23:14:37 -0500

    Typescript type defintion

---
+++ ko-component-router.d.ts
@@ -0,0 +1,77 @@
+interface KoComponentRouterRoute {
+    component: string;
+}
+
+type CallbackResult = void | false | Promise<boolean>;
+type DoneCallback = (boolean) => void;
+
+type KoComponentRouterPromiseMiddleware = (ctx: KoComponentRouterContext) => CallbackResult;
+type KoComponentRouterCallbackMiddleware = (ctx: KoComponentRouterContext, done: DoneCallback) => void;
+type KoComponentRouterLifecycleMiddleware = (ctx: KoComponentRouterContext) => {
+    beforeRender: () => CallbackResult;
+    afterRender: () => CallbackResult;
+    beforeDispose: () => CallbackResult;
+    afterDispose: () => CallbackResult;
+};
+type KoComponentRouterMiddleware = KoComponentRouterPromiseMiddleware
+    | KoComponentRouterCallbackMiddleware
+    | KoComponentRouterLifecycleMiddleware;
+
+interface KoComponentRouterContext {
+    $child: KoComponentRouterContext;
+    $parent: KoComponentRouterContext;
+    addBeforeNavigateCallback(cb: () => CallbackResult);
+    addBeforeNavigateCallback(cb: (done: DoneCallback) => void);
+    canonicalPath: string;
+    element: HTMLElement;
+    fullPath: string;
+    params: any;
+    path: string;
+    pathname: string;
+    route: KoComponentRouterRoute;
+    router: KoComponentRouterInstance;
+}
+
+interface KoComponentRouterInstance {
+    $child: KoComponentRouterInstance;
+    $parent: KoComponentRouterInstance;
+    base: string;
+    ctx: KoComponentRouterContext;
+    isRoot: boolean;
+    isNavigating: KnockoutObservable<boolean>;
+
+    getPathFromLocation(): string;
+    resolvePath(path): any;
+    update(path: string, push?: boolean): Promise<boolean>;
+}
+
+interface KoComponentRouterRoutes {
+    [path: string]: string
+        | KoComponentRouterRoutes
+        | Array<string | KoComponentRouterPromiseMiddleware | KoComponentRouterLifecycleMiddleware>;
+}
+
+declare module 'ko-component-router' {
+    interface RouterStatic extends KoComponentRouterInstance {
+    }
+    namespace RouterStatic {
+        export var config: { base?: string; hashbang?: boolean; };
+        export var middleware: KoComponentRouterMiddleware[];
+        export var plugins: any[];
+        export var routes: KoComponentRouterRoutes;
+        export const head: KoComponentRouterInstance;
+        export const tail: KoComponentRouterInstance;
+
+        export function canonicalizePath(path: string): string;
+        export function get(index: number): KoComponentRouterInstance;
+        export function getPath(url: string): string;
+        export function parseUrl(url: string): { hash: string, pathname: string, search: string };
+        export function sameOrigin(href: string): boolean;
+        export function update(path: string, push?: boolean): Promise<boolean>;
+        export function update(path: string, options: { push?: boolean; force?: boolean; with?: any }): Promise<boolean>;
+        export function use(...middleware: KoComponentRouterMiddleware[]): void;
+        export function usePlugin(...fns): void;    
+    }
+
+    export = RouterStatic;
+}
