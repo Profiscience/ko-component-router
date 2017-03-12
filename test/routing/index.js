f7d4e9aea93da0f58d66e0dec276669a6193b8f9
Casey Webb <notcaseywebb@gmail.com>
2017-03-12 17:51:25 -0500

    WIP

--- test/routing/index.js
+++ test/routing/index.js
@@ -6,32 +6,34 @@ import Router from '../../dist/modules'
 import * as init from './init'
 import * as basic from './basic'
 
-// import params from './params'
-// import nested from './nested'
-// import similar from './similar'
-// import ambiguous from './ambiguous'
-// import _static from './static'
+import * as params from './params'
+import * as nested from './nested'
+import * as similar from './similar'
+import * as ambiguous from './ambiguous'
+
+const tests = [
+  init,
+  basic,
+  params,
+  nested,
+  similar,
+  ambiguous
+]
 
 // const paths = [
-//   '/basic',
 //   // '/params/foo',
 //   // '/nested/a',
 //   // '/similar/foo/bar',
-//   // '/ambiguous/a/b/c',
-//   // '/static'
+//   // '/ambiguous/a/b/c'
 // ]
 
-const paths = flatMap([init, basic], 'paths')
+const paths = flatMap(tests, 'paths')
 
 ko.components.register('routing', {
   template: '<ko-component-router></ko-component-router>',
   viewModel: class RoutingTestSuite {
     constructor({ t, done }) {
-      Router.useRoutes(extend({},
-        init.routes,
-        basic.routes
-      ))
-
+      Router.useRoutes(extend({}, flatMap(tests, 'routes')))
       ko.tasks.schedule(() => this.runTests(t).then(done))
     }
 
